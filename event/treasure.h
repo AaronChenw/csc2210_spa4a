b959f0c114024d90a5c4822da0588b89297c5eb8
Aaron Chen <chenw@msoe.edu>
2024-12-06 11:34:47 -0600

    Combine small classes together.

---
+++ event/treasure.h
@@ -0,0 +1,81 @@
+//
+// Created by eds on 11/23/2024.
+//
+
+#ifndef TREASURE_H
+#define TREASURE_H
+
+#include "../item/weapon.h"
+#include "event.h"
+
+// class item;
+class player;
+class treasure : public event{
+public:
+  bool can_go() override;
+protected:
+  item* its = nullptr;
+  bool is_picked = false;
+};
+
+#endif //TREASURE_H
+
+#ifndef ARROW_TREASURE_H
+#define ARROW_TREASURE_H
+
+class arrow_treasure: public treasure {
+public:
+  arrow_treasure() {
+    its = new arrow();
+  };
+  ~arrow_treasure() override{};
+  // Unpicked: =
+  void show_on_map() override;
+  void interaction(player* p) override;
+
+};
+
+
+
+#endif //ARROW_TREASURE_H
+
+
+#ifndef MEDICINE_TREASURE_H
+#define MEDICINE_TREASURE_H
+
+
+
+class medicine_treasure : public treasure{
+public:
+  medicine_treasure():treasure() {
+    its = new medicine();
+  };
+  ~medicine_treasure() override {}
+  // Unpicked: ^
+  void show_on_map() override;
+  void interaction(player* p) override;
+
+};
+
+
+
+#endif //MEDICINE_TREASURE_H
+
+
+#ifndef SWORD_TREASURE_H
+#define SWORD_TREASURE_H
+
+class sword_treasure final : public treasure{
+public:
+  sword_treasure():treasure() {
+    its = new sword();
+  };
+  ~sword_treasure() override {}
+  // Unpicked: -
+  void show_on_map() override;
+  void interaction(player* p) override;
+};
+
+
+
+#endif //SWORD_TREASURE_H
---
+++ item/weapon.cpp
@@ -0,0 +1,45 @@
+//
+// Created by eds on 12/1/2024.
+//
+
+#include "weapon.h"
+#include "../player/player.h"
+void weapon::benefit_player(player* p) {
+  p->set_weapon(this);
+}
+void melee::weapon_status() {
+  std::cout << name  << "(melee), Attack Point: " << ap << std::endl;
+}
+
+std::string melee::get_weapon_type() {
+  return std::string("melee");
+}
+
+int melee::get_ap(weapon *w) const {
+  int r1 = 0;
+  if (w->get_weapon_type() == "melee") {
+    r1 = ap;
+  }
+  return r1;
+}
+
+
+void long_range::weapon_status() {
+  std::cout << name  << "(long range), Attack Point: " << ap << std::endl;
+}
+
+std::string long_range::get_weapon_type() {
+  return std::string("long_range");
+}
+
+int long_range::get_ap(weapon *w) const {
+  int r1 = 0;
+  if (w->get_weapon_type() == "melee") {
+    r1 = ap;
+  }
+  if (w->get_weapon_type() == "long_range") {
+    r1 = ap / 2;
+  }
+  return r1;
+}
+
